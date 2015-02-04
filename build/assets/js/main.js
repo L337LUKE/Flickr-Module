0f57b0672f8cf5d48ec4b6e1c63ef51e3ca9df22
Luke <luke@lukeroberts.me>
2015-02-04 01:41:04 +0000

    application logic finished

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,77 +1,166 @@
-// globals
-var form = document.getElementById('flickrSearch');
+(function flickr() {
+    // globals
+    var globals = {
+        formEl : document.getElementById('flickrSearch'),
+        // auth
+        apikey : '34f6e33224b098884a1e80a5ccb8e2bb',
+        secret : 'c45369f080495c35',
+        userid : '129226618@N03',
+        // get page els
+        container : document.querySelector('.js-flickr-gallery'),
+        pagination : document.querySelector('.flickr-pagination__js'),
+        input  : document.querySelector('.flickr-search__js'),
+        minSearch : 3,
+    };
 
-// check for submition
-var app = {
-    container : document.querySelector('.js-flickr-gallery'),
-    pagination : document.querySelector('.flickr-pagination__js'),
-    input  : document.querySelector('.flickr-search__js'),
-    apikey : '34f6e33224b098884a1e80a5ccb8e2bb',
-    secret : 'c45369f080495c35',
-    userid : '129226618@N03',
+    //function to remove child elements
+    var removeChilds = function (node) {
+        while (node.lastChild) {
+            node.removeChild(node.lastChild);
+        }
+        return;
+    };
 
-    handleSearch : function(e){
+    var createHandler = function() {
+        return function(event) {
+
+        };
+    };
+
+    // build the gallery
+    var buildGallery = function(imgsrc, ahref) {
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        var img = document.createElement('img');
+
+        // set element attributes attributesq
+        li.className = 'flickr-gallery__item';
+        img.className = 'flickr-gallery__image';
+        a.className = 'flickr-gallery__link';
+
+        a.href = ahref;
+        img.src = imgsrc;
+        // append the children to the container
+        li.appendChild(a).appendChild(img);
+        globals.container.appendChild(li);
+    };
+
+    // build pagination
+    var buildPagination = function() {
+        // declare variables
+        var li,
+            a,
+            span,
+            i = 1;
+
+        // limite page numbers to 10
+        while (i < 11){
+            li = document.createElement('li');
+            a = document.createElement('a');
+            span = document.createElement('span');
+            // add classes
+            li.className = 'flickr-pagination__item';
+            a.className = 'flickr-pagination__link';
+            span.className = 'flickr-pagination__current';
+            // set data atribute
+            a.setAttribute('data-page', i);
+            // apply the index as text to span
+            span.innerText = i;
+            // append to DOM
+            li.appendChild(a).appendChild(span);
+            globals.pagination.insertBefore(li, globals.pagination.firstchild);
+
+            i++;
+        }
+    };
+
+    var handleForm = function (e, searchTerm){
         // prevent default behaviour
-        e.preventDefault();
-        var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+app.apikey+"&tags="+app.input.value+"&privacy_filter=1&safe_search=1&content_type=6&per_page=15&page=1&format=json&nojsoncallback=1";
+        event.preventDefault();
 
-        app.handleRequest(url, function(xhr) {
-            // while container has child, remove it
-            while (app.container.firstChild) {
-                app.container.removeChild(app.container.firstChild);
-                // done!
-            }
+        // validation
+        if(searchTerm === "search" || searchTerm === ""){
+            globals.input.value = "Please enter a valid search Term";
+            return false;
+        }
+        if (globals.input.value.length <= globals.minSearch) {
+            globals.input.value = "Please enter more characters";
+            return false;
+        }
 
+        // set default
+        searchTerm = typeof searchTerm !== 'undefined' ? searchTerm : 'concert';
+
+        // strip whitespace
+        var str = globals.input.value;
+        var searchData = str.replace(/\s+/g, '');
+        // build url
+        var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+globals.apikey+"&tags="+searchData+"&privacy_filter=1&safe_search=1&content_type=6&per_page=15&page=1&format=json&nojsoncallback=1";
+
+        // remove childs
+        removeChilds(globals.container);
+        removeChilds(globals.pagination);
+
+        // run ajax function
+        handleRequest(url, function(xhr) {
+            // parse json
+            var parsed = JSON.parse(xhr);
             // loop through response
             for (var i=0; i<parsed.photos.photo.length; i++){
                 // create variables to iterate on and store imgsrc/href
                 var photo = parsed.photos.photo[i];
                 var imgsrc = 'https://farm'+photo.farm+'.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_m.jpg';
                 var ahref = 'https://www.flickr.com/photos/'+photo.owner+'/'+photo.id+'';
-
+                // fire the build gallery function with img/a src
                 buildGallery(imgsrc, ahref);
             }
         });
+        // build pagination
+        buildPagination();
 
-        buildGallery = function(imgsrc, ahref) {
-            // create html tags
-            var li = document.createElement('li');
-            var a = document.createElement('a');
-            var img = document.createElement('img');
+        var paginEls = document.querySelectorAll('.flickr-pagination__link');
 
-            // set element attributes attributesq
-            li.className = 'flickr-gallery__item';
-            img.className = 'flickr-gallery__image';
-            a.className = 'flickr-gallery__link';
+        // this is used to hold on to the i variable and stop bubbling
+        function paginationClickFunction(evntl, e) {
+            return function (e) {
+                handlePagination(evntl, e);
+                e.stopPropagation();
+            };
+        }
 
-            a.href = ahref;
-           img.src = imgsrc;
+        // loop through adding in click events
+        for (var i = 0, len = paginEls.length; i < len; i++) {
+            paginEls[i].addEventListener('click', paginationClickFunction(i, e) );
+        }
 
-            // append the children to the container
-           li.appendChild(a).appendChild(img);
-           app.container.appendChild(li);
-        };
         // stop the form from searching
         return false;
-    },
-    // handlePagination : function(pageNo) {
-    //
-    // },
-    //handleGallery : function(res, pageNo) {
+    };
 
-        // user clicks page no
-        // user clicks forward/back
-        // user clicks first/last
+    var handlePagination = function(page, e) {
+        // fallback for IE
+        var target = (e.target) ? e.target : e.srcElement;
 
-        //for (p=0; p<) {
+        var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+globals.apikey+"&tags="+globals.input.value+"&privacy_filter=1&safe_search=1&content_type=6&per_page=15&page="+page+"&format=json&nojsoncallback=1";
 
-        //}
+        removeChilds(globals.container);
 
-    //     generatePagination = function() {
-    //
-    //     };
-    // },
-    handleRequest : function(url, callback) {
+        handleRequest(url, function(xhr) {
+            // parse json
+            var parsed = JSON.parse(xhr);
+            // loop through response
+            for (var i=0; i<parsed.photos.photo.length; i++){
+                // create variables to iterate on and store imgsrc/href
+                var photo = parsed.photos.photo[i];
+                var imgsrc = 'https://farm'+photo.farm+'.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_m.jpg';
+                var ahref = 'https://www.flickr.com/photos/'+photo.owner+'/'+photo.id+'';
+                // fire the build gallery function with img/a src
+                buildGallery(imgsrc, ahref);
+            }
+        });
+    };
+
+    var handleRequest = function(url, callback) {
         var xhr;
         // setup request
         xhr = new XMLHttpRequest();
@@ -94,88 +183,19 @@ var app = {
         }
         xhr.open("GET", url, true);
         xhr.send();
-    },
-    // generateView : function(json) {
-    //     // for building the image
-    //     // https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
-    //     // for building the URL
-    //     // https://www.flickr.com/photos/{user-id}/{photo-id} - individual photo
-    //
-    //     // parse the json response
-    //     var rsp = JSON.parse(json);
-    //
-    //     buildGallery = function(imgurl, linkurl) {
-    //         // create html tags
-    //         var li = document.createElement('li');
-    //         var a = document.createElement('a');
-    //         var img = document.createElement('img');
-    //
-    //         // set element attributes attributesq
-    //         li.className = 'flickr-gallery__item';
-    //         img.className = 'flickr-gallery__image';
-    //         a.className = 'flickr-gallery__link';
-    //
-    //         a.href = ahref;
-    //         img.src = imgsrc;
-    //
-    //         // append the children to the container
-    //         li.appendChild(a).appendChild(img);
-    //         app.container.appendChild(li);
-    //     };
-    //
-    //     buildPagination = function(maxPages) {
-    //         // create html tags
-    //         var li = document.createElement('li');
-    //         var a = document.createElement('a');
-    //         var span = document.createElement('span');
-    //
-    //         // add classes
-    //         li.className = 'flickr-pagination__item';
-    //         a.className = 'flickr-pagination__link';
-    //         span.className = 'flickr-pagination__current';
-    //
-    //         // append to DOM
-    //         li.appendChild(a).appendChild(span);
-    //         app.pagination.appendChild(li);
-    //     };
-    //     // While the container has a first child, remove that child
-    //     while (app.container.firstChild) {
-    //         app.container.removeChild(app.container.firstChild);
-    //     }
-    //
-    //     // loop through the returned json and create the list items
-    //
-    // },
-};
+    };
 
-// if (window.addEventListener) {
-//     window.addEventListener("load", app.handleSearch());
-// } else {
-//     window.attachEvent("onload", app.handleSearch());
-// }
-
-// event listener fallback
-if (form.addEventListener) {
-    form.addEventListener("submit", app.handleSearch, false);
-} else {
-    form.attachEvent("onsubmit", app.handleSearch);
-}
-
-
-// user loads page
-// -- grab 15 photos
-// -- loop through json
-// -- output to screen
-// make 1st page element current
-
-// user clicks page 2/3/4/5/ etc
-// performs search again
-// grabs page # of results
-// removes elements, applies to page
-
-// user clicks back / forward
-// grabs next/previous page of results by performing search and adding 1/2
-
-// user clicks first / last
-// does search grabbing first page
-// does search grabbing last page
+    // event listener fallback
+    if (globals.formEl.addEventListener) {
+        // run on load and wait for submission
+        window.addEventListener('load', handleForm, false);
+        globals.formEl.addEventListener("submit", function(e){
+            handleForm(e, globals.input.value);
+        }, false);
+    } else {
+        window.attachEvent("onload", handleForm);
+        globals.formEl.attachEvent("onsubmit", function(e) {
+            handleForm(e, globals.input.value);
+        });
+    }
+})();
